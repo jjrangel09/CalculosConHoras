8d03dc7102d7fc5a575d0c6e74a8f5ff9ff643ca
Juan José Rangel <juanjose840@yahoo.com.co>
2018-02-19 22:56:25 -0500

    Calcula Semanas Dias y Horas
    Programa que con x horas, calcula su proporcion en semanas, dias y horas.

---
+++ CalculosConHoras/src/calculosconhoras/CalculosConHoras.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculosconhoras;
+import java.util.Scanner;
+/**
+ *
+ * @author JuanJ
+ */
+public class CalculosConHoras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int horasIng;
+        int horas;
+        int dias;
+        int semanas;
+        int temporal;
+        int hrDia = 24;
+        int hrSemana = 168;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca el número de horas (entero): ");
+	horasIng = sc.nextInt();
+        
+       temporal = horasIng%hrSemana;
+       semanas = (horasIng-temporal)/hrSemana;
+       horas = temporal;
+       temporal = temporal%hrDia;
+       dias = (horas-temporal)/hrDia;
+       horas = temporal;
+       System.out.println("Semanas: " + semanas);
+       System.out.println("Días: " + dias);
+       System.out.println("Horas: " + horas);
+    }
+    
+}
